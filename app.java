4e2b4e1d3ba9def7c199e9334d53636caa09d39d
Bhargava Reddy Kakunuri <93231991+Bhargavareddykakunuri@users.noreply.github.com>
2025-11-07 00:00:37 +0000

    updated java code by developer-2
    Added a Java program to compute the GCD using the Euclidean algorithm.

--- app.java
+++ app.java
@@ -27,3 +27,29 @@ class GFG {
                            + " is " + GCD(x, y));
     }
 }
+
+
+
+
+//Developer-2
+
+// Java program to find GCD of two 
+// numbers using Euclidean algorithm
+class geeksforgeeks {
+    // Function to return gcd of x and y
+    // recursively
+    static int GCD(int x, int y)
+    {
+        if (y == 0)
+            return x;
+        return GCD(y, x % y);
+    }
+
+    // The Driver code
+    public static void main(String[] args)
+    {
+        int x = 47, y = 91;
+        System.out.println("The GCD of " + x + " and " + y
+                           + " is: " + GCD(x, y));
+    }
+}
